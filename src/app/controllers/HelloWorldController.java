7e697f5991040a39880e35cc97ba24a7f03f9212
Julius Zint <julius.zint@awin-software.de>
2018-10-12 15:41:09 +0200

    added usersession provider action
    added a java action for composition with other controller actions that
    loads if available the usersession and stores it in the context object

--- src/app/controllers/HelloWorldController.java
+++ src/app/controllers/HelloWorldController.java
@@ -1,7 +1,10 @@
 package controllers;
 
+import extension.UserSessionProvider;
+import models.UserSession;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.With;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -11,5 +14,16 @@ public class HelloWorldController extends Controller {
 
     public Result index() {
         return ok("Hello World");
+    }
+
+    @With(UserSessionProvider.class)
+    public Result julius() {
+        UserSession session = (UserSession)ctx().args.get("SessionObject");
+        if(session != null) {
+            return ok("authenticated");
+        }
+        else {
+            return ok("not authenticated");
+        }
     }
 }
---
+++ src/app/extension/UserSessionProvider.java
@@ -0,0 +1,35 @@
+package extension;
+
+import models.UserSession;
+import play.mvc.Http;
+import play.mvc.Result;
+import constants.CookieConstants;
+
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
+
+public class UserSessionProvider extends play.mvc.Action.Simple {
+    public CompletionStage<Result> call(Http.Context ctx) {
+        String sessionIdString = ctx.session().get(CookieConstants.USER_SESSION_ID_NAME);
+        Optional<Integer> sessionId = tryParseInt(sessionIdString);
+        if(sessionId.isPresent()) {
+            Optional<UserSession> userSession = UserSession.findById(sessionId.get());
+            if(userSession.isPresent()) {
+                ctx.args.put("SessionObject", new UserSession());
+            }
+        }
+        return delegate.call(ctx);
+    }
+
+    private Optional<Integer> tryParseInt(String value) {
+        if(value == null) {
+            return Optional.empty();
+        }
+        try {
+            int i = Integer.parseInt(value);
+            return Optional.of(i);
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
+    }
+}
