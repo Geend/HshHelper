3ebce0bf29ff9d48dd886f8120e1d64b6a0f0747
Torben Voltmer <git@t-voltmer.net>
2018-10-12 13:04:44 +0200

    Added UserSessions

---
+++ src/app/models/UserSession.java
@@ -0,0 +1,35 @@
+package models;
+
+import org.joda.time.DateTime;
+
+public class UserSession {
+
+    private Integer sessionId;
+    private Integer userId;
+    private DateTime issuedAt;
+
+
+    public Integer getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(Integer sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public DateTime getIssuedAt() {
+        return issuedAt;
+    }
+
+    public void setIssuedAt(DateTime issuedAt) {
+        this.issuedAt = issuedAt;
+    }
+}
