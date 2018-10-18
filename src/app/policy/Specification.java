3f3c81dd6a3b15dd29cd629d48ba35682a5cf0cc
philip <philip.matesanz@stud.hs-hannover.de>
2018-10-18 23:25:04 +0200

    Policy Specification for first Iteration.

--- src/app/policy/Specification.java
+++ src/app/policy/Specification.java
@@ -2,16 +2,54 @@ package policy;
 
 import models.Group;
 import models.User;
+import play.Logger;
 
 public class Specification {
+    public static boolean CanViewGroupDetails(User currentUser, Group toBeWatched) {
+        if(toBeWatched.members.contains(currentUser)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean CanViewAllGroupsList(User currentUser) {
+        if(currentUser.isAdmin()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean CanCreateUser(User currentUser) {
+        if(currentUser.isAdmin()) {
+            return false;
+        }
+
+        return false;
+    }
+
+    public static boolean CanRemoveUser(User currentUser) {
+        if(currentUser.isAdmin()) {
+            return false;
+        }
+
+        return false;
+    }
+
+    public static boolean CanResetPassword(User currentUser, User toBeResetted) {
+        return true;
+    }
+
     public static boolean CanRemoveGroup(User currentUser, Group group) {
-        if(currentUser.id.equals(group.owner.id)) {
+        if(currentUser.equals(group.owner)) {
             return true;
         }
 
         if(currentUser.isAdmin()) {
             return true;
         }
+
         return false;
     }
 
@@ -29,5 +67,26 @@ public class Specification {
         }
 
         return false;
+    }
+
+    public static boolean CanAddGroupMember(User currentUser, Group group, User toBeAdded) {
+        // No duplicates!
+        if(group.members.contains(toBeAdded)) {
+            return false;
+        }
+
+        if(currentUser.isAdmin()) {
+            return true;
+        }
+
+        if(group.owner.equals(currentUser)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean CanCreateGroup(User currentUser) {
+        return true;
     }
 }
