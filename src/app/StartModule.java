630cc3219ac2f946617a995016e850b7ac176bfb
philip <philip.matesanz@stud.hs-hannover.de>
2018-10-19 14:16:12 +0200

    Implemented a Tasks that periodically purges old/expired sessions from the database.

--- src/app/StartModule.java
+++ src/app/StartModule.java
@@ -1,4 +1,5 @@
 import com.google.inject.AbstractModule;
+import models.cleaners.SessionCleaner;
 import models.finders.UserFinder;
 import models.finders.UserSessionFinder;
 import play.Logger;
@@ -7,7 +8,10 @@ public class StartModule extends AbstractModule {
     protected void configure() {
         Logger.info("DatabaseInitialization");
         bind(DatabaseInitialization.class).asEagerSingleton();
+        bind(SessionCleaner.class).asEagerSingleton();
         bind(UserSessionFinder.class);
         bind(UserFinder.class);
+
+
     }
 }
