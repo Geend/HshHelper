6895e2957614dbbe76af3523b5ed70aaa9131a7b
Philip Matesanz <philip.matesanz@driggle.com>
2018-10-23 23:07:50 +0200

    Run Firewall-GC every 5 Min..

--- src/app/LoginFwInitialization.java
+++ src/app/LoginFwInitialization.java
@@ -1,7 +1,26 @@
+import akka.actor.ActorSystem;
 import policy.ext.loginFirewall.Firewall;
+import scala.concurrent.ExecutionContext;
+import scala.concurrent.duration.Duration;
+
+import javax.inject.Inject;
+import java.util.concurrent.TimeUnit;
 
 public class LoginFwInitialization {
-    public LoginFwInitialization() {
+    private final ActorSystem actorSystem;
+    private final ExecutionContext executionContext;
+
+    @Inject
+    public LoginFwInitialization(ActorSystem actorSystem, ExecutionContext executionContext) {
+        this.actorSystem = actorSystem;
+        this.executionContext = executionContext;
         Firewall.Initialize();
+
+        this.actorSystem.scheduler().schedule(
+                Duration.create(0, TimeUnit.SECONDS), // initialDelay
+                Duration.create(5, TimeUnit.MINUTES), // interval
+                Firewall::GarbageCollect,
+                this.executionContext
+        );
     }
 }
