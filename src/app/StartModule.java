27bfb1d9b512d54fb8cd7336a460553f24167b25
Julius Zint <julius.zint@awin-software.de>
2018-11-02 14:24:44 +0100

    default ebean server can be injected via dependency injection

--- src/app/StartModule.java
+++ src/app/StartModule.java
@@ -1,6 +1,9 @@
 import com.google.inject.AbstractModule;
-import models.finders.UserFinder;
+import io.ebean.Ebean;
+import io.ebean.EbeanServer;
 import play.Logger;
+
+import javax.inject.Provider;
 
 public class StartModule extends AbstractModule {
     protected void configure() {
@@ -8,5 +11,12 @@ public class StartModule extends AbstractModule {
         bind(DatabaseInitialization.class).asEagerSingleton();
         bind(LoginFwInitialization.class).asEagerSingleton();
         bind(SessionInitialization.class).asEagerSingleton();
+        bind(EbeanServer.class).toProvider(new EbeanServerProvider());
+    }
+
+    public class EbeanServerProvider implements Provider<EbeanServer> {
+        public EbeanServer get() {
+            return Ebean.getDefaultServer();
+        }
     }
 }
