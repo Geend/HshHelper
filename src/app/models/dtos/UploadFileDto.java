b291560c9835daff8443c44b4e92702e91968480
philip <philip.matesanz@stud.hs-hannover.de>
2018-11-15 17:22:16 +0100

    Added rudimentary functionality to upload files

---
+++ src/app/models/dtos/UploadFileDto.java
@@ -0,0 +1,22 @@
+package models.dtos;
+
+public class UploadFileDto {
+    private String filename;
+    private String comment;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
