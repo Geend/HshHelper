72955259110bbea8418a9d631498327e509a4e5a
Julius Zint <julius.zint@awin-software.de>
2018-11-16 15:44:56 +0100

    renamed groupmemberpolicytests to specificationtests

--- src/test/SpecificationTests.java
+++ src/test/SpecificationTests.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class GroupMemberPolicyTests {
+public class SpecificationTests {
 
     private static User admin;
     private static User adminTwo;
