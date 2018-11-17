4a2c8cca7f5696e650127da719d86543fb6872af
philip <philip.matesanz@stud.hs-hannover.de>
2018-11-17 10:21:58 +0100

    Added rudimentary functionality to show files with granted access

--- src/app/controllers/HomeController.java
+++ src/app/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import managers.filemanager.FileManager;
+import models.File;
 import models.User;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -9,21 +11,26 @@ import policyenforcement.session.SessionManager;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import java.util.List;
+
 import static play.libs.Scala.asScala;
 
 @Singleton
 @Authentication.Required
 public class HomeController extends Controller {
     private final SessionManager sessionManager;
+    private final FileManager fileManager;
 
     @Inject
-    public HomeController(SessionManager sessionManager) {
+    public HomeController(SessionManager sessionManager, FileManager fileManager) {
         this.sessionManager = sessionManager;
+        this.fileManager = fileManager;
     }
 
     public Result index() {
         User user = sessionManager.currentUser();
-        return ok(views.html.Index.render(user, asScala(user.getGroups())));
+        List<File> accessibleFiles = fileManager.accessibleFiles(sessionManager.currentUser().getUserId());
+        return ok(views.html.Index.render(user, asScala(user.getGroups()), asScala(accessibleFiles)));
     }
 
 }
