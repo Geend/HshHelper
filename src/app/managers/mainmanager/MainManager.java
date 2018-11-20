5f149b6bad88b7b2e6b5a3c371aad1c3bacd2fb5
Julius Zint <julius.zint@awin-software.de>
2018-11-20 17:18:36 +0100

    user quota in main view

--- src/app/managers/mainmanager/MainManager.java
+++ src/app/managers/mainmanager/MainManager.java
@@ -1,18 +1,55 @@
 package managers.mainmanager;
 
+import models.User;
+import models.finders.FileFinder;
 import models.finders.UserFinder;
+import models.finders.UserQuota;
+import policyenforcement.session.SessionManager;
 
 import javax.inject.Inject;
 
 public class MainManager {
     private UserFinder userFinder;
+    private FileFinder fileFinder;
+    private SessionManager sessionManager;
 
     @Inject
-    public MainManager(UserFinder userFinder) {
+    public MainManager(SessionManager sessionManager, UserFinder userFinder, FileFinder fileFinder) {
+        this.sessionManager = sessionManager;
         this.userFinder = userFinder;
+        this.fileFinder = fileFinder;
     }
 
     public String getQuotaUsage() {
-        return "alles verbraucht";
+        User currentUser = this.sessionManager.currentUser();
+        UserQuota userQuota = this.fileFinder.getUsedQuota(currentUser.getUserId());
+        double totalUsage = userQuota.getTotalUsage();
+        double userQuotaLimit = currentUser.getQuotaLimit();
+        int percentage = (int)((100.0 / userQuotaLimit) * totalUsage);
+        return String.format("%s %% aufgebraucht. (%s, von %s belegt)", percentage, formatSize(totalUsage), formatSize(userQuotaLimit));
+    }
+
+    private String formatSize(double sizeInByte) {
+        String[] names = new String[8];
+        names[0] = "Byte";
+        names[1] = "Kilobyte";
+        names[2] = "Megabyte";
+        names[3] = "Gigabyte";
+        names[4] = "Terabyte";
+        names[5] = "Petabyte";
+        names[6] = "Exabyte";
+        names[7] = "Zettabyte";
+
+        int finalIndex = 0;
+        for(int i = 0; i < 6; i++) {
+            if(sizeInByte > 1000) {
+                sizeInByte = sizeInByte / 1000.0;
+                finalIndex++;
+            }
+            else {
+                break;
+            }
+        }
+        return String.format("%s %s", sizeInByte, names[finalIndex]);
     }
 }
