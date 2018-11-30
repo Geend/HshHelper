1040f11233451a916f0fca4f8dbb3c491b2a178a
philip <philip.matesanz@stud.hs-hannover.de>
2018-11-30 10:42:38 +0100

    encryption class to be created by torben

---
+++ src/app/extension/CryptoHelper.java
@@ -0,0 +1,12 @@
+package extension;
+
+public class CryptoHelper {
+
+    public byte[] encrypt(String key, byte[] plaintext) {
+        return null;
+    }
+
+    public byte[] decrypt(String key, byte[] cyphertext) {
+        return null;
+    }
+}
